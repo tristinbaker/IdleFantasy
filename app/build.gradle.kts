3920647b10130a2b241baa657fcdfdc7b80602f5
Tristin Baker <tristinbaker52@gmail.com>
2026-05-14 09:39:10 -0400

    Boot recipes, tiered ashes, quest fixes, crafting filters, skill descriptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tristinbaker.idlefantasy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.4"
+        versionCode = 12
+        versionName = "1.1.5"
     }
 
     dependenciesInfo {
